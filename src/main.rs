52f8e294fa546f3faf6ab13ccf0d19f34ffee39b
Stefano Baghino <stefano@baghino.me>
2026-04-20 09:31:41 +0200

    Wire up the CLI binary
    See src/main.rs. Reads from an argv path or stdin; embeds
    grammars/json.peg via include_str!.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    eprintln!("syntax-highlighter: CLI not implemented yet");
+use std::io::{self, Read, Write};
+use std::process::ExitCode;
+
+use syntax_highlighter::highlight::Highlighter;
+
+const JSON_GRAMMAR: &str = include_str!("../grammars/json.peg");
+
+fn main() -> ExitCode {
+    let mut args = std::env::args().skip(1);
+    let input = match args.next() {
+        Some(path) => match std::fs::read_to_string(&path) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("syntax-highlighter: {}: {}", path, e);
+                return ExitCode::from(2);
+            }
+        },
+        None => {
+            let mut buf = String::new();
+            if let Err(e) = io::stdin().read_to_string(&mut buf) {
+                eprintln!("syntax-highlighter: reading stdin: {}", e);
+                return ExitCode::from(2);
+            }
+            buf
+        }
+    };
+
+    let highlighter = match Highlighter::new(JSON_GRAMMAR) {
+        Ok(h) => h,
+        Err(e) => {
+            eprintln!("syntax-highlighter: grammar error: {}", e);
+            return ExitCode::from(3);
+        }
+    };
+
+    let out = highlighter.highlight(&input);
+    if let Err(e) = io::stdout().write_all(out.as_bytes()) {
+        eprintln!("syntax-highlighter: writing output: {}", e);
+        return ExitCode::from(2);
+    }
+    ExitCode::SUCCESS
 }
